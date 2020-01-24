869b190e970e74e79c4f15c37569732e22f39f0c
Maria Balueva <mbalueva@gmail.com>
2020-01-24 13:35:05 +0300

    новая схема базы данных

--- scheme.sql
+++ scheme.sql
@@ -6,20 +6,35 @@ USE taskforce;
 
 CREATE TABLE cities (
   id int PRIMARY KEY AUTO_INCREMENT,
-  name varchar(255) NOT NULL
+  name varchar(30) NOT NULL
 );
 
 CREATE TABLE categories (
   id int PRIMARY KEY AUTO_INCREMENT,
-  name varchar(255) NOT NULL
+  name varchar(40) NOT NULL
+);
+
+CREATE TABLE `statuses` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(20)
+);
+
+CREATE TABLE `actions` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(20)
+);
+
+CREATE TABLE `roles` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(20)
 );
 
 CREATE TABLE users (
   id int PRIMARY KEY AUTO_INCREMENT,
-  email varchar(255) NOT NULL,
-  name varchar(255) NOT NULL,
+  email varchar(40) NOT NULL,
+  name varchar(30) NOT NULL,
   citi_id int NOT NULL,
-  password varchar(255) NOT NULL,
+  password varchar(64) NOT NULL,
   address varchar(255),
   latitude float,
   longitude float,
@@ -27,18 +42,11 @@ CREATE TABLE users (
   avatar_path varchar(255),
   date_birth date,
   about_itself text,
-  phone varchar(255),
-  skype varchar(255),
-  other_messanger varchar(255),
   last_activity datetime,
   popularity int,
-  notify_chat tinyint(1) DEFAULT 1,
-  notify_actions tinyint(1) DEFAULT 1,
-  notify_rates tinyint(1) DEFAULT 1,
-  show_contacts tinyint(1) DEFAULT 1,
-  show_profile tinyint(1) DEFAULT 1,
-  role ENUM ('onwer', 'doer'),
-  FOREIGN KEY (citi_id) REFERENCES cities (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  `role_id` int NOT NULL,
+  FOREIGN KEY (citi_id) REFERENCES cities (id) ON UPDATE CASCADE,
+  FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
   UNIQUE INDEX (email),
   INDEX (id),
   INDEX (name),
@@ -57,16 +65,18 @@ CREATE TABLE tasks (
   doer_id int,
   expires_at datetime,
   created_at timestamp DEFAULT CURRENT_TIMESTAMP,
-  status ENUM ('new', 'canceled', 'in_progress', 'failed', 'done') NOT NULL,
+  status_id int NOT NULL,
   category_id int NOT NULL,
-  FOREIGN KEY (citi_id) REFERENCES cities (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (doer_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (citi_id) REFERENCES cities (id) ON UPDATE CASCADE,
+  FOREIGN KEY (owner_id) REFERENCES users (id) ON UPDATE CASCADE,
+  FOREIGN KEY (doer_id) REFERENCES users (id) ON UPDATE CASCADE,
+  FOREIGN KEY (status_id) REFERENCES statuses (id)ON UPDATE CASCADE,
+  FOREIGN KEY (category_id) REFERENCES categories (id) ON UPDATE CASCADE,
   INDEX (id),
   INDEX (title),
   INDEX (citi_id),
   INDEX (expires_at),
+  INDEX (status_id),
   INDEX (category_id)
 );
 
@@ -75,8 +85,8 @@ CREATE TABLE users_specializations (
   id int PRIMARY KEY AUTO_INCREMENT,
   user_id int NOT NULL,
   category_id int NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE,
+  FOREIGN KEY (category_id) REFERENCES categories (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE responses (
@@ -87,8 +97,8 @@ CREATE TABLE responses (
   price int NOT NULL,
   created_at timestamp DEFAULT CURRENT_TIMESTAMP,
   is_refused tinyint(1),
-  FOREIGN KEY (task_id) REFERENCES tasks (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (task_id) REFERENCES tasks (id) ON UPDATE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE,
   INDEX (task_id)
 );
 
@@ -101,8 +111,8 @@ CREATE TABLE rates (
   rate tinyint(5),
   ready_status tinyint(1),
   created_at timestamp DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (task_id) REFERENCES tasks (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (task_id) REFERENCES tasks (id) ON UPDATE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE,
   INDEX (user_id)
 );
 
@@ -113,15 +123,15 @@ CREATE TABLE messages (
   to_user_id int NOT NULL,
   message text NOT NULL,
   created_at timestamp DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (task_id) REFERENCES tasks (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (from_user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (to_user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (task_id) REFERENCES tasks (id) ON UPDATE CASCADE,
+  FOREIGN KEY (from_user_id) REFERENCES users (id) ON UPDATE CASCADE,
+  FOREIGN KEY (to_user_id) REFERENCES users (id) ON UPDATE CASCADE,
   INDEX (task_id),
   INDEX (from_user_id),
   INDEX (to_user_id)
 );
 
-CREATE TABLE files (
+CREATE TABLE `tasks_files` (
   id int PRIMARY KEY AUTO_INCREMENT,
   task_id int NOT NULL,
   img_path varchar(255) NOT NULL,
@@ -133,6 +143,57 @@ CREATE TABLE favorites (
   id int PRIMARY KEY AUTO_INCREMENT,
   user_id int NOT NULL,
   fovorite_profile_id int NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (fovorite_profile_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE,
+  FOREIGN KEY (fovorite_profile_id) REFERENCES users (id) ON UPDATE CASCADE,
+  INDEX (user_id)
+);
+
+CREATE TABLE `users_contacts` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `phone` varchar(20),
+  `skype` varchar(30),
+  `other` varchar(100),
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
+  INDEX (`user_id`)
+);
+
+CREATE TABLE `users_avatars` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `path` varchar(255) NOT NULL,
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
+  INDEX (`user_id`)
+);
+
+CREATE TABLE `users_notifications_types` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(100)
+);
+
+CREATE TABLE `users_notifications_settings` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `notification_type_id` int NOT NULL,
+  `value` boolean,
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
+  FOREIGN KEY (`notification_type_id`) REFERENCES `users_notifications_types` (`id`) ON UPDATE CASCADE,
+  INDEX (`user_id`)
+);
+
+CREATE TABLE `users_visibility_settings` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `show_profile` boolean,
+  `show_contacts` boolean,
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
+  INDEX (`user_id`)
+);
+
+CREATE TABLE `users_works_images` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `path` varchar(255),
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
+  INDEX (`user_id`)
 );
